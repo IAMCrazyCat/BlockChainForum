cd68f3060020dc7573cb08135508a639bc5ec124
IAMCrazyCat <liuzimingjay@vip.qq.com>
2021-09-24 23:35:55 +1000

    Created New project

---
+++ src/main/java/com/blockchainform/BlockchainformApplication.java
@@ -0,0 +1,13 @@
+package com.blockchainform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockchainformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlockchainformApplication.class, args);
+    }
+
+}
