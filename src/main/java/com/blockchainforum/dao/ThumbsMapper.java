bd5531ace5a62ab0692d1ba05ecb92953dbf8c38
tqiu2998 <tqiu2998@uni.sydney.edu.au>
2021-10-16 16:08:23 +1100

    feature: thumb and comment controller optimization

--- src/main/java/com/blockchainforum/dao/ThumbsMapper.java
+++ src/main/java/com/blockchainforum/dao/ThumbsMapper.java
@@ -3,9 +3,12 @@ package com.blockchainforum.dao;
 import com.blockchainforum.entity.Thumbs;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface ThumbsMapper {
     Thumbs selectById(int tid, int uid, int pid);
-    int insertThumbs(Thumbs thumbs);
-    int deleteThumbs(int tid, int uid, int pid);
+    int insertThumbs(int tid, int uid, int pid);
+    int deleteThumbs(int tid);
+    List<Thumbs> selectThumbsByUid(int uid);
 }
