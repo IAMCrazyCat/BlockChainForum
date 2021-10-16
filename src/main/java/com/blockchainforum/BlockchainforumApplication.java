da17d600360c25782e2e4bd3154a3bed2cbf4086
tqiu2998 <tqiu2998@uni.sydney.edu.au>
2021-10-16 16:10:29 +1100

    release: prepare for backend integration

--- src/main/java/com/blockchainforum/BlockchainforumApplication.java
+++ src/main/java/com/blockchainforum/BlockchainforumApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlockchainforumApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BlockchainforumApplication.class, args);
     }
